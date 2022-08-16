be9decb8b104fc38027aaa5bae8ae13f15304ab3
baidoolot <bdtbaidoolot@gmail.com>
2022-08-16 17:54:05 +0600

    Django-1

---
+++ clients/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+# clients/admin.py
+from .models import Client
+
+
+admin.site.register(Client)
---
+++ clients/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+from .models import Client
+
+def contacts(request):
+    return HttpResponse("<h1>Тел.: 0706257085   ||  0707702072  ||  0999668392</h1>")
+
+def info(request):
+    return render(request, "info.html")
+
+def clients_list(request):
+    context = {}
+    bottles_list = Client.objects.all()
+    context["clients_list"] = bottles_list
+    return render(request, 'clients.html', context)
+
